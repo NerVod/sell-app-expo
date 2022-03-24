af54a87941ba78a5d98decbf7b0506fce2611919
NerVod <benjamin.jeannerot@gmail.com>
2022-03-24 10:58:36 +0100

    improve listItem component , image and subtitle facultative

--- App.js
+++ App.js
@@ -11,11 +11,15 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItems from "./app/components/ListItem";
 
 export default function App() {
 return (
     <Screen>
-        <Icon name="email" size={100}/>
+        <ListItems 
+        title="myTitle" 
+        imageComponent={<Icon name='email' />}
+        />
     </Screen>
     );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,7 +5,7 @@ import AppText from './AppText/AppText';
 import { Swipeable, GestureHandlerRootView } from 'react-native-gesture-handler';
 import colors from '../config/colors';
 
-function ListItems({title, subTitle, image, onPress, renderRightActions}) {
+function ListItems({title, subTitle, image, imageComponent, onPress, renderRightActions}) {
     return (
         <GestureHandlerRootView >
             <Swipeable renderRightActions={renderRightActions}>
@@ -13,10 +13,11 @@ function ListItems({title, subTitle, image, onPress, renderRightActions}) {
                 underlayColor={colors.light}
                 onPress={onPress}>
                     <View style ={styles.container}>
-                        <Image style={styles.image} source={image} />
-                        <View>
+                        {imageComponent}
+                        {image && <Image style={styles.image} source={image} />}
+                        <View style={styles.detailsContainer}>
                             <AppText style={styles.title}>{title}</AppText>
-                            <AppText style={styles.subTitle}>{subTitle}</AppText>
+                            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
                         </View>
                     </View>
                 </TouchableHighlight>
@@ -30,11 +31,14 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         padding: 15,
     },
+    detailsContainer: {
+        marginLeft: 10,
+        justifyContent:"center",
+    },
     image:{
         width: 70,
         height: 70,
         borderRadius: 35,
-        marginRight:10,
     },
     title: {
         fontWeight: "500",
