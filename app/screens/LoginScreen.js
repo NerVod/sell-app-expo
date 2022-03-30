f8e168a48e51d62b60faa82c6565ebf7fdc5b7a3
NerVod <benjamin.jeannerot@gmail.com>
2022-03-30 16:20:43 +0200

    touched item only have error message login screen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -24,28 +24,30 @@ function LoginScreen(props) {
             onSubmit={values => console.log(values)}
             validationSchema={validationSchema}
             >
-                { ({ handleChange, handleSubmit, errors}) => (
+                { ({ handleChange, handleSubmit, errors, setFieldTouched, touched}) => (
                         <>
                         <AppTextInput 
                         autoCapitalize="none"
                         autoCorrect={false}
                         keybordType="email-address"
+                        onBlur={() =>setFieldTouched("email")}
                         onChangeText={handleChange("email")}
                         icon="email"
                         placeholder="Email"
                         textContentType="emailAddress"
                         />
-                        <ErrorMessage error={errors.email}/>
+                        <ErrorMessage error={errors.email} visible={touched.email} />
                         <AppTextInput 
                         autoCapitalize="none"
                         autoCorrect={false}
                         icon='lock'
+                        onBlur={() =>setFieldTouched("password")}
                         onChangeText={handleChange("password")}
                         placeholder="Password"
                         secureTextEntry={true}
                         textContentType="password"
                         />
-                        <ErrorMessage error={errors.password}/>
+                        <ErrorMessage error={errors.password} visible={touched.password}/>
                         <AppButton title="login" onPress={handleSubmit} color="primary"/>
                         
                         </>
